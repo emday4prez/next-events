aaba6df494d924e606e509e6299c9fe91251e811
Emerson <emerson.day@gmail.com>
2022-10-22 15:05:20 -0700

    optimized data fetching

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,5 +1,9 @@
 import { Fragment } from 'react';
-import { getEventById, getAllEvents } from '../../helpers/api-util';
+import {
+  getEventById,
+  getAllEvents,
+  getFeaturedEvents,
+} from '../../helpers/api-util';
 import EventSummary from '../../components/event-detail/event-summary';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventContent from '../../components/event-detail/event-content';
@@ -10,9 +14,9 @@ function EventDetailPage(props) {
 
   if (!event) {
     return (
-      <ErrorAlert>
-        <p>no event found</p>;
-      </ErrorAlert>
+      <div className="center">
+        <p>Loading...</p>;
+      </div>
     );
   }
 
@@ -41,15 +45,16 @@ export async function getStaticProps(context) {
     props: {
       selectedEvent: event,
     },
+    revalidate: 30,
   };
 }
 
 export async function getStaticPaths() {
-  const events = await getAllEvents();
+  const events = await getFeaturedEvents();
   const path = events.map((event) => ({ params: { eventId: event.id } }));
   return {
     paths: path,
-    fallback: false,
+    fallback: true,
   };
 }
 

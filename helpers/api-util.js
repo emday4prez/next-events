244dcb64df3bdf05796582476fa4acdc4bcad9aa
Emerson <emerson.day@gmail.com>
2022-10-22 14:19:28 -0700

    added more static props

--- helpers/api-util.js
+++ helpers/api-util.js
@@ -20,3 +20,8 @@ export async function getAllEvents() {
 
   return events;
 }
+
+export async function getEventById(id) {
+  const allEvents = await getAllEvents();
+  return allEvents.find((event) => event.id === id);
+}
